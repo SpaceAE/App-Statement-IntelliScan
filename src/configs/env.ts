ff5c20f683e6595e66e98146656a0697159780c7
Kanesh Orachunlertmitri <kanesh132@gmail.com>
2025-09-29 15:18:57 +0700

    [feat] add mui and env validate

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -3,13 +3,20 @@ import z from 'zod';
 
 export const env = createEnv({
   server: {
-    SECRET_KEY: z.string().nonempty().describe('The secret key for the app'),
+    SECRET_KEY: z.string().optional().describe('The secret key for the app'),
   },
   client: {
-    NEXT_PUBLIC_API_URL: z.url().describe('The base URL for the API'),
+    NEXT_PUBLIC_API_URL: z
+      .url()
+      .optional()
+      .describe('The base URL for the API'),
   },
   runtimeEnv: {
     SECRET_KEY: process.env.SECRET_KEY,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  onValidationError: issue => {
+    console.error(' \x1b[31m✗\x1b[0m Invalid environment variables:', issue);
+    process.exit(1);
   },
 });
