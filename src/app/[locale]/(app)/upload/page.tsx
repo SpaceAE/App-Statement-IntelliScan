9d02e3537fb8fcb4d51d2264ffcf853c0f6be02e
Kanesh Orachunlertmitri <kanesh132@gmail.com>
2025-10-01 18:00:00 +0700

    [feat] implement upload page

--- src/app/[locale]/(app)/upload/page.tsx
+++ src/app/[locale]/(app)/upload/page.tsx
@@ -1,9 +1,44 @@
+'use client';
+
+import { Button, Stack, Typography } from '@mui/material';
+import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+
 import InputFileUpload from '@/components/InputFileUpload';
 
 export default function UploadPage() {
+  const t = useTranslations('upload');
+  const [file, setFile] = useState<File | null>(null);
+
   return (
-    <div>
-      <InputFileUpload acceptedTypes=".pdf" maxSize={10} />
-    </div>
+    <Stack
+      justifyContent={'flex-start'}
+      sx={{
+        padding: '10vh 12px 0',
+        height: '100%',
+        gap: '32px',
+        '@media (max-height: 800px)': {
+          paddingTop: '12px',
+          gap: '16px',
+        },
+      }}
+    >
+      <Typography
+        component={'h1'}
+        variant="h4"
+        fontWeight={'bold'}
+        textAlign={'center'}
+      >
+        {t('title')}
+      </Typography>
+      <InputFileUpload
+        acceptedTypes=".pdf"
+        maxSize={10}
+        onFileSelect={(selectedFile: File) => setFile(selectedFile)}
+      />
+      <Button variant="contained" color="primary" disabled={!file}>
+        {t('uploadButton')}
+      </Button>
+    </Stack>
   );
 }
