6f146c905c7ad173c83d1a0f21ac870423222462
Kanesh Orachunlertmitri <kanesh132@gmail.com>
2025-10-04 14:00:43 +0700

    [feat] add config default theme

--- src/plugins/mui/Provider.tsx
+++ src/plugins/mui/Provider.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 
+import { env } from '@/config/env';
+
 import theme from './theme';
 
 export default function MuiProvider({
@@ -9,7 +11,10 @@ export default function MuiProvider({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider theme={theme} defaultMode="light">
+    <ThemeProvider
+      theme={theme}
+      defaultMode={env.NEXT_PUBLIC_DEFAULT_THEME_MODE}
+    >
       <CssBaseline />
       {children}
     </ThemeProvider>
